af95aeb7f51dc58a8ed25433743ddb27254981a1
Shivam Ranjan <shivam.ranjan@truemeds.in>
2024-06-12 16:05:03 +0530

    SL-01 chrome calc extension

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Calculator } from "./components/calculator";
+
+function App() {
+  return (
+    <div className="container w-[25rem] p-1 bg-zinc-800">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
